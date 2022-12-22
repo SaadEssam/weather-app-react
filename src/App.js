02b0fd4a1cb23d7ec6f1015dc8f0fb966f5f7a00
SaadEssam <SaadEssam059@gmail.com>
2022-12-22 16:53:02 +0200

    city search component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
+import Search from './components/search/search'
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '49d40008afmsh28d1da212a59c4bp1d6de9jsn74fe1643e21f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
