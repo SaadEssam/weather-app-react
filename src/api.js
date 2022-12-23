1d0ef40afd8ce4441dfffb12fa0b7a85425be1e6
SaadEssam <SaadEssam059@gmail.com>
2022-12-23 17:03:14 +0200

    Fetching data from weather api

--- src/api.js
+++ src/api.js
@@ -2,9 +2,11 @@ export const geoApiOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '49d40008afmsh28d1da212a59c4bp1d6de9jsn74fe1643e21f',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a45a502779e593b1cb58cfa0397da3e3";
